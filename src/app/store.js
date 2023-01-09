e00349b8686c21febd90fcf38ddc59363e61d531
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-10 02:16:15 +0600

    configureStore> reducer: > counter:counterSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "../features/counter/counterSlice";
 
 const store = configureStore({
     devTools: true,
     reducer: {
-
+        counter: counterSlice,
     },
 });
+
+export default store;
